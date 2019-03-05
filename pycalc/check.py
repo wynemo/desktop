bd7bfb94e15f5fdd1fa4aa848c57a22d0b16ef1c
nemo.zhang <nemo.zhang@axel.org>
2019-03-05 21:04:26 +0800

    execute python script

--- pycalc/check.py
+++ pycalc/check.py
@@ -4,3 +4,5 @@ rule_path = sys.argv[1]
 with open(rule_path, 'r') as f:
     for line in f.readlines():
         print(line)
+with open('C:/Users/zdb/build/check/2.txt', 'w') as f:
+    f.write('123')
---
+++ readme.txt
@@ -0,0 +1,3 @@
+ electron-packager . --overwrite --asar
+ C:\Python27\Scripts\pyinstaller.exe --onefile C:\Users\zdb\Documents\private\desktop\pycalc\check.py
+ 
