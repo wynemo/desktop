b5b530940a343f34c334d7911a67f86353f73c69
Nemo Zhang <nemo.zhang@stoamigo.com>
2019-03-06 20:44:39 +0800

    read file

--- pycalc/check.py
+++ pycalc/check.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 state_stopped = 0
@@ -35,7 +36,8 @@ def read_config(path):
                     configs[current_name] = []
                     state = state_rule
                 else:
-                    print('ignore rule line', line)
+                    #print('ignore rule line', line)
+                    pass
             elif line.startswith(keyword_prefix) or line.startswith(splitword_prefix) or line.startswith(
                     calc_prefix) or line.startswith(standard_prefix):
                 if state in (state_rule, state_rule_stopped):
@@ -47,17 +49,62 @@ def read_config(path):
                         configs[current_name].append(current_config)
                         current_config = {}
                 else:
-                    print('ignore config line', line)
+                    #print('ignore config line', line)
+                    pass
             else:
-                print('ignore line', line)
+                #print('ignore line', line)
+                pass
     return configs
 
 
-def read_file(file_path):
-    pass
+def check_result(calc, standard, result):
+    if not standard.isdigit():
+        standard = '\'' + standard + '\''
+    if not result.isdigit():
+        result = '\'' + result + '\''
+    s = result + calc + standard
+    return eval(s)
+
+
+def read_file(file_path, configs):
+    pattern = '\<[\w-]+?\>(.+)'
+    current_rule = None
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line:
+                continue
+            o = re.search(pattern, line)
+            if o is not None:
+                rule = o.group(1)
+                rule = rule.strip()
+                if rule in configs:
+                    current_rule = rule
+                else:
+                    current_rule = None
+            else:
+                if current_rule is not None:
+                    check_rules = configs[current_rule]
+                    for check_rule in check_rules:
+                        keyword = check_rule['keyword']
+                        o = re.match(keyword, line)
+                        if o is not None:
+                            keyword_result = o.group()
+                            splitword_pattern = check_rule['splitword']
+                            o = re.search(splitword_pattern, line)
+                            if o is not None:
+                                splitword = o.group()
+                                pos = line.find(splitword)
+                                result = line[pos + len(splitword):]
+                                result = result.strip()
+                                if check_rule['standard']:
+                                    standard = check_rule['standard']
+                                    result = check_result(check_rule['calc'], standard, result)
+                                print('path is %s, rule is %s, keyword is %s, result is %s' % (file_path, current_rule, keyword_result, result))
 
 
 if __name__ == '__main__':
     rule_path = sys.argv[1]
     configs = read_config(rule_path)
-    print(configs)
+    file_path = sys.argv[2]
+    read_file(file_path, configs)
